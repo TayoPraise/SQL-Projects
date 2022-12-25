6e6dfd3742d59161dae435d89186da7d74d90cb1
TayoPraise <107925747+TayoPraise@users.noreply.github.com>
2022-12-25 19:54:24 +0100

    Add files via upload

---
+++ webscrapping.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_txt = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+
+soup = BeautifulSoup(html_txt, 'lxml')
+job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+
+print(company_name)
+
