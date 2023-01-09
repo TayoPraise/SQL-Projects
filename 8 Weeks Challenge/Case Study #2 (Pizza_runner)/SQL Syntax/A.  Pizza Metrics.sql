a2df962731bab83422c8fb79ae8bfa382d87fc9b
Temitayo Ipinlaiye <107925747+TayoPraise@users.noreply.github.com>
2023-01-09 09:30:27 +0100

    Create A.  Pizza Metrics.sql

---
+++ 8 Weeks Challenge/Case Study #2 (Pizza_runner)/SQL Syntax/A.  Pizza Metrics.sql
@@ -0,0 +1,104 @@
+-- 1. How many pizzas were ordered?
+
+SELECT COUNT(pizza_id) "Number of pizzas ordered"
+FROM pizza_runner.cl_customer_orders;
+
+-- 2. How many unique customer orders were made?
+
+SELECT COUNT(DISTINCT order_id) "Number of unique order"
+FROM pizza_runner.cl_customer_orders;
+
+-- 3. How many successful orders were delivered by each runner?
+
+SELECT runner_id
+	   , COUNT(order_id) "Numbers of orders delivered"
+FROM pizza_runner.cl_runner_orders
+WHERE cancellation = ''
+GROUP BY 1;
+
+-- 4. How many of each type of pizza was delivered?
+
+WITH delivery AS (
+      SELECT order_id
+           , cancellation
+      FROM pizza_runner.cl_runner_orders
+      WHERE cancellation = ''
+      )
+
+SELECT pizza_name
+	   , COUNT(pizza_id) "Number of pizzas delivered"
+FROM delivery
+JOIN pizza_runner.cl_customer_orders co
+USING(order_id)
+JOIN pizza_runner.pizza_names pn
+USING(pizza_id)
+GROUP BY 1
+ORDER BY 1;
+
+-- 5. How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT customer_id
+   , pizza_name
+   , count(pizza_id) "number of pizzas ordered"
+FROM pizza_runner.cl_customer_orders
+JOIN pizza_runner.pizza_names
+USING(pizza_id)
+GROUP BY 1, 2
+ORDER BY 1;
+
+-- 6. What was the maximum number of pizzas delivered in a single order?
+
+WITH pizza_count AS (
+		SELECT order_id
+			  , Count(pizza_id) "pizza_count"
+		FROM pizza_runner.cl_runner_orders
+		JOIN pizza_runner.cl_customer_orders
+		USING(order_id)
+		WHERE cancellation = ''
+		GROUP BY 1
+	     )	 
+	
+	SELECT MAX(pizza_count)"max pizzas delivered in a single order"
+	FROM pizza_count;
+
+-- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+SELECT customer_id
+	   , SUM(CASE WHEN exclusions <> '' OR extras <> ''
+				THEN 1 ELSE 0
+			 END) "order with changes"
+	   , SUM(CASE WHEN exclusions = '' AND extras = ''
+				THEN 1 ELSE 0
+			 END) "orders without changes"
+FROM pizza_runner.cl_customer_orders
+JOIN pizza_runner.cl_runner_orders
+USING (order_id)
+WHERE cancellation = ''
+GROUP BY 1
+ORDER BY 1;
+
+-- 8. How many pizzas were delivered that had both exclusions and extras?
+
+SELECT SUM(CASE WHEN exclusions <> '' AND extras <> ''
+			THEN 1 ELSE 0
+		 END) "order with both exclusions & extra"
+FROM pizza_runner.cl_customer_orders
+JOIN pizza_runner.cl_runner_orders
+USING (order_id)
+WHERE cancellation = '';
+
+-- 9. What was the total volume of pizzas ordered for each hour of the day?
+
+SELECT EXTRACT(HOURS FROM order_time) hours
+	 , COUNT(order_id) "pizza ordered"
+FROM pizza_runner.cl_customer_orders
+GROUP BY 1
+ORDER BY 1;
+
+-- 10. What was the volume of orders for each day of the week?
+
+SELECT to_char(order_time, 'DAY')
+	 , COUNT(order_id) "pizza ordered"
+FROM pizza_runner.cl_customer_orders
+GROUP BY 1
+ORDER BY 2 DESC;
