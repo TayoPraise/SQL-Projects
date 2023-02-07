73c5846003e91922f585eedbaee58d8d548f4679
Temitayo Ipinlaiye <107925747+TayoPraise@users.noreply.github.com>
2023-02-07 19:16:49 +0100

    Update A.  Pizza Metrics.sql

--- 8 Weeks Challenge/Case Study #2 (Pizza_runner)/SQL Syntax/A.  Pizza Metrics.sql
+++ 8 Weeks Challenge/Case Study #2 (Pizza_runner)/SQL Syntax/A.  Pizza Metrics.sql
@@ -4,18 +4,18 @@ SET SEARCH_PATH = pizza_runner;
 -- 1. How many pizzas were ordered?
 
 SELECT COUNT(pizza_id) "Number of pizzas ordered"
-FROM pizza_runner.cl_customer_orders;
+FROM cl_customer_orders;
 
 -- 2. How many unique customer orders were made?
 
 SELECT COUNT(DISTINCT order_id) "Number of unique order"
-FROM pizza_runner.cl_customer_orders;
+FROM cl_customer_orders;
 
 -- 3. How many successful orders were delivered by each runner?
 
 SELECT runner_id
 	   , COUNT(order_id) "Numbers of orders delivered"
-FROM pizza_runner.cl_runner_orders
+FROM cl_runner_orders
 WHERE cancellation = ''
 GROUP BY 1;
 
@@ -24,16 +24,16 @@ GROUP BY 1;
 WITH delivery AS (
       SELECT order_id
            , cancellation
-      FROM pizza_runner.cl_runner_orders
+      FROM cl_runner_orders
       WHERE cancellation = ''
       )
 
 SELECT pizza_name
 	   , COUNT(pizza_id) "Number of pizzas delivered"
 FROM delivery
-JOIN pizza_runner.cl_customer_orders co
+JOIN cl_customer_orders co
 USING(order_id)
-JOIN pizza_runner.pizza_names pn
+JOIN pizza_names pn
 USING(pizza_id)
 GROUP BY 1
 ORDER BY 1;
@@ -43,8 +43,8 @@ ORDER BY 1;
 SELECT customer_id
    , pizza_name
    , count(pizza_id) "number of pizzas ordered"
-FROM pizza_runner.cl_customer_orders
-JOIN pizza_runner.pizza_names
+FROM cl_customer_orders
+JOIN pizza_names
 USING(pizza_id)
 GROUP BY 1, 2
 ORDER BY 1;
@@ -54,8 +54,8 @@ ORDER BY 1;
 WITH pizza_count AS (
 		SELECT order_id
 			  , Count(pizza_id) "pizza_count"
-		FROM pizza_runner.cl_runner_orders
-		JOIN pizza_runner.cl_customer_orders
+		FROM cl_runner_orders
+		JOIN cl_customer_orders
 		USING(order_id)
 		WHERE cancellation = ''
 		GROUP BY 1
@@ -73,8 +73,8 @@ SELECT customer_id
 	   , SUM(CASE WHEN exclusions = '' AND extras = ''
 				THEN 1 ELSE 0
 			 END) "orders without changes"
-FROM pizza_runner.cl_customer_orders
-JOIN pizza_runner.cl_runner_orders
+FROM cl_customer_orders
+JOIN cl_runner_orders
 USING (order_id)
 WHERE cancellation = ''
 GROUP BY 1
@@ -85,8 +85,8 @@ ORDER BY 1;
 SELECT SUM(CASE WHEN exclusions <> '' AND extras <> ''
 			THEN 1 ELSE 0
 		 END) "order with both exclusions & extra"
-FROM pizza_runner.cl_customer_orders
-JOIN pizza_runner.cl_runner_orders
+FROM cl_customer_orders
+JOIN cl_runner_orders
 USING (order_id)
 WHERE cancellation = '';
 
@@ -94,7 +94,7 @@ WHERE cancellation = '';
 
 SELECT EXTRACT(HOURS FROM order_time) hours
 	 , COUNT(order_id) "pizza ordered"
-FROM pizza_runner.cl_customer_orders
+FROM cl_customer_orders
 GROUP BY 1
 ORDER BY 1;
 
@@ -102,6 +102,6 @@ ORDER BY 1;
 
 SELECT to_char(order_time, 'DAY')
 	 , COUNT(order_id) "pizza ordered"
-FROM pizza_runner.cl_customer_orders
+FROM cl_customer_orders
 GROUP BY 1
 ORDER BY 2 DESC;
